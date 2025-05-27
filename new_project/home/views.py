12efe8a6b107aba076f303cbb14e71ea61ebfa97
felixmathew03 <felixmathewtt@gmail.com>
2025-05-27 16:24:07 +0530

    added booking option

--- new_project/home/views.py
+++ new_project/home/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .models import Departments, Doctors
+from .forms import BookingForm
 # Create your views here.
 def index(request):
-    numbers={
-        'num1': [1,2,3,4,5,6]
-    }
-    return render(request,"index.html",numbers)
+    return render(request,"index.html")
 
 def about(request):
     return render(request,"about.html")
 
 def booking(request):
-    return render(request,"bookings.html")
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():    
+            form.save()
+            return render(request,'confirmation.html')
+    form=BookingForm()
+    dict_form={
+        "form" : form
+    }
+    return render(request,"bookings.html",dict_form)
 
 def doctors(request):
     dict_docs={
